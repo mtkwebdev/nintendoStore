4d0a1a80ae1cdb12298a471dab58574f8a494e34
mtkwebdev <62560333+mtkwebdev@users.noreply.github.com>
2021-05-30 00:49:29 +0100

    deploy mobile optimisations

--- src/Components/Layout/Header/menuData.js
+++ src/Components/Layout/Header/menuData.js
@@ -1,5 +1,10 @@
 import React from 'react'
-import nswitch from '../../../static/images/menuIcons/switch.png'
+import nswitch from '../../../static/images/svg/switch.svg'
+import games from '../../../static/images/svg/games.svg'
+import eShop from '../../../static/images/svg/eshop.svg'
+import support from '../../../static/images/svg/support.svg'
+import myStore from '../../../static/images/svg/mystore.svg'
+import news from '../../../static/images/svg/news.svg'
 
 
 export const menuData = [
@@ -10,32 +15,27 @@ export const menuData = [
     },
     {
         title:"Games",
-        icon: <img className="menuIcon" src={nswitch} alt=""/>,
-        link: "/menuLocation"
+        icon: <img className="menuIcon" src={games} alt=""/>,
+        link: "/games"
     },
     {
         title:"Nintendo eShop",
-        icon: <img className="menuIcon" src={nswitch} alt=""/>,
-        link: "https://www.nintendo.co.uk/Nintendo-eShop/Nintendo-eShop-1806894.html"
+        icon: <img className="menuIcon" src={eShop} alt=""/>,
+        link: "/eshop"
     },
     {
         title:"Support",
-        icon: <img className="menuIcon" src={nswitch} alt=""/>,
-        link: "/menuLocation"
+        icon: <img className="menuIcon" src={support} alt=""/>,
+        link: "/support"
     },
     {
         title:"My Nintendo Store",
-        icon: <img className="menuIcon" src={nswitch} alt=""/>,
-        link: "/menuLocation"
+        icon: <img className="menuIcon" src={myStore} alt=""/>,
+        link: "/eshop"
     },
     {
         title:"News",
-        icon: <img className="menuIcon" src={nswitch} alt=""/>,
-        link: "/menuLocation"
-    },
-    {
-        title:"Menu Item",
-        icon: <img className="menuIcon" src={nswitch} alt=""/>,
-        link: "/menuLocation"
+        icon: <img className="menuIcon" src={news} alt=""/>,
+        link: "/news"
     },
 ]
